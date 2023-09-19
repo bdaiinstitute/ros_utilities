1a9e508ef799ed3d74b7cba0581cb5e902bb3873
Anthony Baker <abaker@theaiinstitute.com>
2023-09-19 10:48:19 -0600

    remove dependency on bdai.utilites

--- bdai_ros2_wrappers/bdai_ros2_wrappers/rosbag_recorder.py
+++ bdai_ros2_wrappers/bdai_ros2_wrappers/rosbag_recorder.py
@@ -2,10 +2,8 @@
 import os
 import threading
 from datetime import datetime
-from typing import Optional
 
 import rclpy
-from bdai.utilities.path import get_bdai_path
 from rosbag2_py import Recorder, RecordOptions, StorageOptions
 
 ROS_BAG_NAME_TIMESTAMP_FORMAT: str = "rosbag2_%Y_%m_%d-%H_%M_%S_%f"
@@ -14,8 +12,7 @@ ROS_BAG_NAME_TIMESTAMP_FORMAT: str = "rosbag2_%Y_%m_%d-%H_%M_%S_%f"
 class ROSBagRecorder(object):
     def __init__(
         self,
-        bag_file_name: str,
-        bag_file_dir: Optional[str] = None,
+        bag_file_path: str,
         storage_id: str = "sqlite3",
     ) -> None:
         self._rosbag_recorder = Recorder()
@@ -23,16 +20,7 @@ class ROSBagRecorder(object):
         self._record_options = RecordOptions()
         self._record_options.all = True
 
-        bag_file_name += "_"
-
-        self._node_name = bag_file_name + "recorder"
-
-        dt = datetime.now().strftime(ROS_BAG_NAME_TIMESTAMP_FORMAT)
-        bag_name = "{}{}".format(bag_file_name, dt)
-        if bag_file_dir:
-            self._bag_path = os.path.join(bag_file_dir, bag_name)
-        else:
-            self._bag_path = os.path.join(get_bdai_path(), "recordings", bag_name)
+        self._bag_path = "{}_{}".format(bag_file_path, datetime.now().strftime(ROS_BAG_NAME_TIMESTAMP_FORMAT))
 
         self._storage_options = StorageOptions(uri=self._bag_path, storage_id=storage_id)
 
