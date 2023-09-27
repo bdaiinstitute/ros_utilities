c2a9dfdccd6e258c6679c79f94ca8a021123613f
Michel Hidalgo <mhidalgo@theaiinstitute.com>
2023-09-27 09:37:47 -0300

    Update action server tests
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

--- bdai_ros2_wrappers/test/test_single_goal_action_server.py
+++ bdai_ros2_wrappers/test/test_single_goal_action_server.py
@@ -1,75 +1,53 @@
 # Copyright (c) 2023 Boston Dynamics AI Institute Inc.  All rights reserved.
 import array
-import unittest
-from typing import Optional
+from typing import Iterable
 
+import pytest
 import rclpy
 from action_tutorials_interfaces.action import Fibonacci
-from rclpy import Context
 from rclpy.action.server import ServerGoalHandle
-from rclpy.callback_groups import CallbackGroup, ReentrantCallbackGroup
 
 from bdai_ros2_wrappers.action_client import ActionClientWrapper
-from bdai_ros2_wrappers.node import NodeWrapper
+from bdai_ros2_wrappers.process import ROSAwareScope
 from bdai_ros2_wrappers.single_goal_action_server import SingleGoalActionServer
 
 
-def execute_callback(goal_handle: ServerGoalHandle) -> Fibonacci.Result:
+@pytest.fixture
+def ros() -> Iterable[ROSAwareScope]:
+    rclpy.init()
+    try:
+        with ROSAwareScope("fixture") as scope:
+            yield scope
+    finally:
+        rclpy.try_shutdown()
+
+
+def test_single_goal_action_server(ros: ROSAwareScope) -> None:
     """
-    Executor for normal fibonacci sequence
+    Tests normal operation of a single action server
     """
-    sequence = [0, 1]
-    for i in range(1, goal_handle.request.order):
-        sequence.append(sequence[i] + sequence[i - 1])
 
-    goal_handle.succeed()
-
-    result = Fibonacci.Result()
-    result.sequence = sequence
-    return result
-
-
-class SingleGoalActionServerTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.context: Optional[Context] = Context()
-        rclpy.init(context=self.context)
-        self.group: CallbackGroup = ReentrantCallbackGroup()
-        self.action_server_node: NodeWrapper = NodeWrapper(
-            "server", context=self.context, num_executor_threads=2, spin_thread=True
-        )
-        self.action_server: SingleGoalActionServer = SingleGoalActionServer(
-            self.action_server_node,
-            Fibonacci,
-            "fibonacci",
-            execute_callback=execute_callback,
-            callback_group=self.group,
-        )
-        self.client: ActionClientWrapper = ActionClientWrapper(
-            Fibonacci, "fibonacci", "test_action_client", context=self.context
-        )
-
-    def tearDown(self) -> None:
-        if self.client is not None:
-            self.client = None
-        if self.action_server is not None:
-            self.action_server.destroy()
-            self.action_server = None
-        if self.action_server_node is not None:
-            self.action_server_node.shutdown()
-
-        rclpy.shutdown(context=self.context)
-        self.context = None
-
-    def test_single_goal_action_server(self) -> None:
+    def execute_callback(goal_handle: ServerGoalHandle) -> Fibonacci.Result:
         """
-        Tests normal operation of a single action server
+        Executor for normal fibonacci sequence
         """
-        goal = Fibonacci.Goal()
-        goal.order = 5
-        expected_result = array.array("i", [0, 1, 1, 2, 3, 5])
-        result = self.client.send_goal_and_wait("test_single_goal_action_server", goal=goal, timeout_sec=2)
-        self.assertEqual(result.sequence, expected_result)
+        sequence = [0, 1]
+        for i in range(1, goal_handle.request.order):
+            sequence.append(sequence[i] + sequence[i - 1])
 
+        goal_handle.succeed()
 
-if __name__ == "__main__":
-    unittest.main()
+        result = Fibonacci.Result()
+        result.sequence = sequence
+        return result
+
+    SingleGoalActionServer(ros.node, Fibonacci, "fibonacci", execute_callback)
+    action_client = ActionClientWrapper(Fibonacci, "fibonacci", ros.node)
+
+    goal = Fibonacci.Goal()
+    goal.order = 5
+    result = action_client.send_goal_and_wait("test_single_goal_action_server", goal=goal, timeout_sec=2)
+    assert result is not None
+
+    expected_result = array.array("i", [0, 1, 1, 2, 3, 5])
+    assert result.sequence == expected_result
--- bdai_ros2_wrappers/test/test_single_goal_multiple_action_servers.py
+++ bdai_ros2_wrappers/test/test_single_goal_multiple_action_servers.py
@@ -1,25 +1,21 @@
 # Copyright (c) 2023 Boston Dynamics AI Institute Inc.  All rights reserved.
 import array
 import time
-import unittest
-from threading import Thread
-from typing import Optional
+from typing import Iterable, Tuple
 
+import pytest
 import rclpy
 from action_tutorials_interfaces.action import Fibonacci
-from rclpy import Context
 from rclpy.action.server import GoalStatus, ServerGoalHandle
-from rclpy.callback_groups import CallbackGroup, ReentrantCallbackGroup
 
 from bdai_ros2_wrappers.action_client import ActionClientWrapper
-from bdai_ros2_wrappers.node import NodeWrapper
+from bdai_ros2_wrappers.process import ROSAwareScope
 from bdai_ros2_wrappers.single_goal_multiple_action_servers import SingleGoalMultipleActionServers
 
 
 def execute_callback(goal_handle: ServerGoalHandle) -> Fibonacci.Result:
     """
-    Executor callback for a server that does
-    fibonacci
+    Executor callback for a server that does fibonacci
     """
     sequence = [0, 1]
     for i in range(1, goal_handle.request.order):
@@ -34,8 +30,7 @@ def execute_callback(goal_handle: ServerGoalHandle) -> Fibonacci.Result:
 
 def execute_callback_wrong_fib(goal_handle: ServerGoalHandle) -> Fibonacci.Result:
     """
-    Different executor for another server that does
-    fibonacci wrong
+    Different executor for another server that does fibonacci wrong
     """
     # time delay to make interrupting easier
     time.sleep(1)
@@ -55,82 +50,74 @@ def execute_callback_wrong_fib(goal_handle: ServerGoalHandle) -> Fibonacci.Resul
     return result
 
 
-def threaded_client_request(client: ActionClientWrapper) -> None:
+@pytest.fixture
+def ros() -> Iterable[ROSAwareScope]:
+    rclpy.init()
+    try:
+        with ROSAwareScope("fixture") as scope:
+            yield scope
+    finally:
+        rclpy.try_shutdown()
+
+
+@pytest.fixture
+def action_triplet(
+    ros: ROSAwareScope,
+) -> Tuple[SingleGoalMultipleActionServers, ActionClientWrapper, ActionClientWrapper]:
+    action_parameters = [
+        (Fibonacci, "fibonacci", execute_callback, None),
+        (Fibonacci, "fibonacci_wrong", execute_callback_wrong_fib, None),
+    ]
+    action_server = SingleGoalMultipleActionServers(ros.node, action_parameters)
+    action_client_a = ActionClientWrapper(Fibonacci, "fibonacci", ros.node)
+    action_client_b = ActionClientWrapper(Fibonacci, "fibonacci_wrong", ros.node)
+    return action_server, action_client_a, action_client_b
+
+
+def test_actions_in_sequence(
+    action_triplet: Tuple[SingleGoalMultipleActionServers, ActionClientWrapper, ActionClientWrapper]
+) -> None:
     """
-    Threaded client request process to allow testing of multithreading tests
+    Tests out normal operation with multiple action servers and clients
     """
-    # time delay to give other action time to get started before interrupting
-    time.sleep(0.3)
+    _, action_client_a, action_client_b = action_triplet
     goal = Fibonacci.Goal()
     goal.order = 5
-    client.send_goal_and_wait("threaded_client_request", goal=goal, timeout_sec=2)
+    # use first client
+    result = action_client_a.send_goal_and_wait("action_request_a", goal=goal, timeout_sec=5)
+    assert result is not None
+    expected_result = array.array("i", [0, 1, 1, 2, 3, 5])
+    assert result.sequence == expected_result
+    # use second client
+    result = action_client_b.send_goal_and_wait("action_request_b", goal=goal, timeout_sec=5)
+    assert result is not None
+    expected_result = array.array("i", [0, 1, 0, 0, 0, 0])
+    assert result.sequence == expected_result
 
 
-class SingleGoalMultipleActionServerTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.context: Optional[Context] = Context()
-        rclpy.init(context=self.context)
-        self.group: CallbackGroup = ReentrantCallbackGroup()
-        self.action_server_node: Optional[NodeWrapper] = NodeWrapper(
-            "server", context=self.context, num_executor_threads=2, spin_thread=True
-        )
-        self.action_params = [
-            (Fibonacci, "fibonacci", execute_callback, self.group),
-            (Fibonacci, "fibonacci_wrong", execute_callback_wrong_fib, self.group),
-        ]
-        self.action_server = SingleGoalMultipleActionServers(self.action_server_node, self.action_params)
-        self.client = ActionClientWrapper(Fibonacci, "fibonacci", "test_action_client", context=self.context)
-        self.client2 = ActionClientWrapper(Fibonacci, "fibonacci_wrong", "test_wrong_fib_client", context=self.context)
+def test_action_interruption(
+    ros: ROSAwareScope, action_triplet: Tuple[SingleGoalMultipleActionServers, ActionClientWrapper, ActionClientWrapper]
+) -> None:
+    """
+    This test should start a delayed request from another client
+    then make an immediate request to interrupt the last request.
 
-    def tearDown(self) -> None:
-        if self.client is not None:
-            self.client = None
-        if self.client2 is not None:
-            self.client2 = None
-        if self.action_server is not None:
-            self.action_server.destroy()
-            self.action_server = None
-        if self.action_server_node is not None:
-            self.action_server_node.shutdown()
+    Due to the threading and reliance on sleeps this test might be
+    tempermental on other machines.
+    """
+    _, action_client_a, action_client_b = action_triplet
 
-        rclpy.shutdown(context=self.context)
-        self.context = None
-
-    def test_single_goal_action_server(self) -> None:
-        """
-        Tests out normal operation with multiple action servers and clients
-        """
+    def deferred_request() -> None:
+        # time delay to give other action time to get started before interrupting
+        time.sleep(0.3)
         goal = Fibonacci.Goal()
         goal.order = 5
-        expected_result = array.array("i", [0, 1, 1, 2, 3, 5])
-        # use first client
-        result = self.client.send_goal_and_wait("test_single_goal_action_server", goal=goal, timeout_sec=5)
-        self.assertEqual(result.sequence, expected_result)
-        # use second client
-        expected_result = array.array("i", [0, 1, 0, 0, 0, 0])
-        result = self.client2.send_goal_and_wait("test_single_goal_action_server2", goal=goal, timeout_sec=5)
-        self.assertEqual(result.sequence, expected_result)
+        action_client_a.send_goal_and_wait("deferred_action_request", goal=goal, timeout_sec=2)
 
-    def test_interrupted_action(self) -> None:
-        """
-        This test should start a delayed request from another client
-        then make an immediate request to interrupt the last request.
+    ros.executor.create_task(deferred_request)
 
-        Due to the threading and reliance on sleeps this test might be tempermental on other machines
-        """
-        goal = Fibonacci.Goal()
-        goal.order = 5
-        # set up thread that will interrupt this request
-        thread = Thread(target=threaded_client_request, args=(self.client,))
-        thread.start()
-        # immediately start the request for other goal
-        result = self.client2.send_goal_and_wait("test_interrupted_action", goal=goal, timeout_sec=5)
-        # Request is interrupted, so result is None
-        self.assertEqual(result, None)
-
-        # wait for thread to finish
-        thread.join()
-
-
-if __name__ == "__main__":
-    unittest.main()
+    # immediately start the request for other goal
+    goal = Fibonacci.Goal()
+    goal.order = 5
+    result = action_client_b.send_goal_and_wait("action_request", goal=goal, timeout_sec=5)
+    assert result is None
