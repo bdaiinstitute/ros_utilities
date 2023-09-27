5b2d270590c94f0eed53b0e0c2cd12f228dde3e4
Michel Hidalgo <mhidalgo@theaiinstitute.com>
2023-09-27 09:38:06 -0300

    Migrate ActionClientWrapper
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

--- bdai_ros2_wrappers/test/test_action_client.py
+++ bdai_ros2_wrappers/test/test_action_client.py
@@ -1,46 +1,31 @@
 # Copyright (c) 2023 Boston Dynamics AI Institute Inc.  All rights reserved.
 import array
 import time
-import unittest
-from threading import Thread
-from typing import Optional
+from typing import Any, Iterable
 
+import pytest
 import rclpy
 from action_tutorials_interfaces.action import Fibonacci
-from rclpy import Context
-from rclpy.action import ActionServer
-from rclpy.action.server import GoalResponse, ServerGoalHandle, default_goal_callback
-from rclpy.executors import ExternalShutdownException, MultiThreadedExecutor
-from rclpy.node import Node
+from rclpy.action.server import ActionServer, GoalResponse, ServerGoalHandle
 
 from bdai_ros2_wrappers.action_client import ActionClientWrapper
+from bdai_ros2_wrappers.node import Node
+from bdai_ros2_wrappers.process import ROSAwareScope
 
 
-def do_not_accept_goal(goal_request: Fibonacci.Goal) -> GoalResponse:
-    """
-    Helper callback function for rejecting goals to help test
-    """
-    return GoalResponse.REJECT
-
-
-class FibonacciActionServer(Node):
+class FibonacciActionServer(ActionServer):
     """
     Action server to used for testing mostly pulled from ROS2 Action Server tutorial
 
     Some changes made to allow special testing of timeouts and goal rejections
     """
 
-    def __init__(self, name: str, context: Context, test_reject: bool = False) -> None:
-        super().__init__("fibonacci_action_server", context=context)
-        goal_callback = default_goal_callback if not test_reject else do_not_accept_goal
-        self._action_server = ActionServer(self, Fibonacci, name, self.execute_callback, goal_callback=goal_callback)
-        self._executor: MultiThreadedExecutor = MultiThreadedExecutor(num_threads=2, context=context)
-        self._executor.add_node(self)
-        self._thread: Optional[Thread] = Thread(target=self._spin)
-        self._thread.start()
+    def __init__(self, node: Node, name: str, **kwargs: Any) -> None:
+        super().__init__(node, Fibonacci, name, self.execute_callback, **kwargs)
+        self._logger = node.get_logger()
 
     def execute_callback(self, goal_handle: ServerGoalHandle) -> Fibonacci.Result:
-        self.get_logger().info("Executing goal...")
+        self._logger.info("Executing goal...")
         # sleep to test timeout
         time.sleep(2)
 
@@ -55,97 +40,63 @@ class FibonacciActionServer(Node):
         result.sequence = sequence
         return result
 
-    def _spin(self) -> None:
-        if self._executor is not None:
-            try:
-                self._executor.spin()
-            except (ExternalShutdownException, KeyboardInterrupt):
-                pass
 
-    def stop(self) -> None:
-        if self._executor is not None:
-            self._executor.shutdown()
-            self._executor.remove_node(self)
-            self._executor = None
-        if self._thread is not None:
-            self._thread.join()
-            self._thread = None
-        self._action_server = None
+@pytest.fixture
+def ros() -> Iterable[ROSAwareScope]:
+    rclpy.init()
+    try:
+        with ROSAwareScope("fixture") as scope:
+            yield scope
+    finally:
+        rclpy.try_shutdown()
 
 
-class ActionClientWrapperTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.context: Optional[Context] = Context()
-        rclpy.init(context=self.context)
+def test_send_goal_and_wait(ros: ROSAwareScope) -> None:
+    """
+    Test standard operation of send_goal_and_wait
+    """
+    FibonacciActionServer(ros.node, "fibonacci")
+    action_client = ActionClientWrapper(Fibonacci, "fibonacci", ros.node)
 
-        self.fib_action_server: Optional[FibonacciActionServer] = FibonacciActionServer(
-            name="fibonacci", context=self.context
-        )
-        # create a server that will reject a goal
-        self.fib_reject_server: Optional[FibonacciActionServer] = FibonacciActionServer(
-            name="reject_fib", context=self.context, test_reject=True
-        )
-        self.action_client_wrapper: ActionClientWrapper = ActionClientWrapper(
-            Fibonacci, "fibonacci", "test_action_client", context=self.context
-        )
-        self.action_client_rejected_wrapper: ActionClientWrapper = ActionClientWrapper(
-            Fibonacci, "reject_fib", "test_rejected", context=self.context
-        )
+    goal = Fibonacci.Goal()
+    goal.order = 5
+    result = action_client.send_goal_and_wait("test_send_goal_and_wait", goal=goal, timeout_sec=5)
+    assert result is not None
+    expected_result = array.array("i", [0, 1, 1, 2, 3, 5])
+    assert result.sequence == expected_result
 
-    def tearDown(self) -> None:
-        if self.fib_action_server is not None:
-            self.fib_action_server.stop()
-            self.fib_action_server = None
 
-        if self.fib_reject_server is not None:
-            self.fib_reject_server.stop()
-            self.fib_reject_server = None
+def test_timeout_send_goal_wait(ros: ROSAwareScope) -> None:
+    """
+    Test out the timeout of the send_goal_and_wait
+    """
+    FibonacciActionServer(ros.node, "fibonacci")
+    action_client = ActionClientWrapper(Fibonacci, "fibonacci", ros.node)
 
-        if self.action_client_wrapper is not None:
-            self.action_client_wrapper = None
+    goal = Fibonacci.Goal()
+    goal.order = 5
+    result = action_client.send_goal_and_wait("test_timeout_send_goal_wait", goal=goal, timeout_sec=0.5)
 
-        if self.action_client_rejected_wrapper is not None:
-            self.action_client_rejected_wrapper = None
+    # times out and since action client wrapper does not start its own thread
+    # it uses rclpy spin_until_future_complete which will return none
+    assert result is None
 
-        rclpy.shutdown(context=self.context)
-        self.context = None
 
-    def test_send_goal_and_wait(self) -> None:
+def test_goal_not_accepted(ros: ROSAwareScope) -> None:
+    """
+    Test for the goal not accepted pathway should return None
+    """
+
+    def do_not_accept_goal(goal_request: Fibonacci.Goal) -> GoalResponse:
         """
-        Test standard operation of send_goal_and_wait
+        Helper callback function for rejecting goals to help test
         """
-        goal = Fibonacci.Goal()
-        goal.order = 5
-        expected_result = array.array("i", [0, 1, 1, 2, 3, 5])
-        result = self.action_client_wrapper.send_goal_and_wait("test_send_goal_and_wait", goal=goal, timeout_sec=5)
-        self.assertEqual(result.sequence, expected_result)
+        return GoalResponse.REJECT
 
-    def test_timeout_send_goal_wait(self) -> None:
-        """
-        Test out the timeout of the send_goal_and_wait
-        """
-        goal = Fibonacci.Goal()
-        goal.order = 5
+    FibonacciActionServer(ros.node, "fibonacci", goal_callback=do_not_accept_goal)
+    action_client = ActionClientWrapper(Fibonacci, "fibonacci", ros.node)
 
-        result = self.action_client_wrapper.send_goal_and_wait(
-            "test_timeout_send_goal_wait", goal=goal, timeout_sec=0.5
-        )
-        # times out and since action client wrapper does not start its own thread
-        # it uses rclpy spin_until_future_complete which will return none
-        self.assertEqual(result, None)
-
-    def test_goal_not_accepted(self) -> None:
-        """
-        Test for the goal not accepted pathway should return None
-        """
-        goal = Fibonacci.Goal()
-        goal.order = 5
-
-        result = self.action_client_rejected_wrapper.send_goal_and_wait(
-            "test_goal_not_accepted", goal=goal, timeout_sec=5
-        )
-        self.assertEqual(result, None)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    goal = Fibonacci.Goal()
+    goal.order = 5
+    result = action_client.send_goal_and_wait("test_goal_not_accepted", goal=goal, timeout_sec=5)
+    assert result is None
