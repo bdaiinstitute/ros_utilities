c684dae1c32511c5c74c58ae230e37dcc113e473
mhidalgo-bdai <144129882+mhidalgo-bdai@users.noreply.github.com>
2023-10-04 15:05:38 -0300

    Adds `namespace` utility (#26)

--- bdai_ros2_wrappers/bdai_ros2_wrappers/utilities.py
+++ bdai_ros2_wrappers/bdai_ros2_wrappers/utilities.py
@@ -3,6 +3,11 @@
 import typing
 
 
+def namespace(*args: typing.Optional[str]) -> str:
+    """Puts together a ROS 2 like namespace from its constitutive parts."""
+    return "/".join(arg.strip("/") for arg in filter(None, args))
+
+
 def either_or(obj: typing.Any, name: str, default: typing.Any) -> typing.Any:
     """
     Gets either an object attribute's value or a default value.
