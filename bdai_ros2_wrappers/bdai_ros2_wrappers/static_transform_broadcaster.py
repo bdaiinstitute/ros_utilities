9ce3a7f47c677cb26a48ae63d22439ded6f305ae
mhidalgo-bdai <144129882+mhidalgo-bdai@users.noreply.github.com>
2023-10-23 08:46:27 -0300

    Re-implement StaticTransformBroadcaster to persist transforms (#36)
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

---
+++ bdai_ros2_wrappers/bdai_ros2_wrappers/static_transform_broadcaster.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2023 Boston Dynamics AI Institute Inc.  All rights reserved.
+from typing import Dict, Iterable, Optional, Union, cast
+
+from geometry_msgs.msg import TransformStamped
+from rclpy.node import Node
+from rclpy.qos import DurabilityPolicy, HistoryPolicy, QoSProfile
+from tf2_msgs.msg import TFMessage
+
+
+class StaticTransformBroadcaster:
+    """
+    A modified :class:`tf2_ros.StaticTransformBroadcaster` that stores transforms
+    sent through it, matching ``rclcpp::StaticTransformBroadcaster`` behavior.
+    """
+
+    DEFAULT_QOS = QoSProfile(depth=1, durability=DurabilityPolicy.TRANSIENT_LOCAL, history=HistoryPolicy.KEEP_LAST)
+
+    def __init__(self, node: Node, qos: Optional[Union[QoSProfile, int]] = None) -> None:
+        """
+        Constructor.
+
+        :param node: The ROS2 node.
+        :param qos: A QoSProfile or a history depth to apply to the publisher.
+        """
+        if qos is None:
+            qos = StaticTransformBroadcaster.DEFAULT_QOS
+        self._net_transforms: Dict[str, TransformStamped] = {}
+        self._pub = node.create_publisher(TFMessage, "/tf_static", qos)
+
+    def sendTransform(self, transform: Union[TransformStamped, Iterable[TransformStamped]]) -> None:
+        try:
+            transforms = list(transform)
+        except TypeError:  # in which case, transform is not iterable
+            transforms = [cast(TransformStamped, transform)]
+        self._net_transforms.update({tf.child_frame_id: tf for tf in transforms})
+        self._pub.publish(TFMessage(transforms=list(self._net_transforms.values())))
