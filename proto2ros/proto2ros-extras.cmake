5e927be92b3f8152b2989a09d2f54facbf5bb713
Michel Hidalgo <mhidalgo@theaiinstitute.com>
2024-03-05 11:36:39 -0300

    Set CMP0148 for proto2ros packages
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

--- proto2ros/proto2ros-extras.cmake
+++ proto2ros/proto2ros-extras.cmake
@@ -2,13 +2,13 @@
 include("${proto2ros_DIR}/rosidl_helpers.cmake")
 
 find_package(Python3 REQUIRED)
-find_package(PythonInterp REQUIRED)  # to prevent failure deep down rosidl
 find_package(Protobuf REQUIRED)
 if(BUILD_TESTING)
   find_package(ament_cmake_mypy QUIET)
 endif()
 include("${proto2ros_DIR}/proto2ros_generate.cmake")
 
+cmake_policy(SET CMP0148 OLD)  # to accomodate rosidl pipeline
 find_package(builtin_interfaces REQUIRED)
 find_package(rosidl_default_generators REQUIRED)
 include("${proto2ros_DIR}/proto2ros_vendor_package.cmake")
