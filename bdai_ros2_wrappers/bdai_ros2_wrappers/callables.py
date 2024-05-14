bb32b1008aeb095c4e8a218f3726a025829542ad
Michel Hidalgo <mhidalgo@theaiinstitute.com>
2024-05-14 10:30:42 -0300

    Add generalized methods
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

--- bdai_ros2_wrappers/bdai_ros2_wrappers/callables.py
+++ bdai_ros2_wrappers/bdai_ros2_wrappers/callables.py
@@ -2,10 +2,12 @@
 
 import abc
 import functools
-from typing import Any, Callable, Iterable, Optional, Tuple
+import inspect
+from typing import Any, Callable, Iterable, Optional, Tuple, Type, Union, overload
 
 from rclpy.task import Future
 
+from bdai_ros2_wrappers.executors import assign_coroutine
 from bdai_ros2_wrappers.futures import AnyFuture, FutureLike, as_proper_future
 from bdai_ros2_wrappers.utilities import take_kwargs
 
@@ -163,3 +165,189 @@ class ComposedCallable(GeneralizedDecorator, ComposableCallable, VectorizingCall
         if self.starred:
             return self.wrapped_callable.asynchronous(*self.composed_callable(*args, **inner_kwargs), **outer_kwargs)
         return self.wrapped_callable.asynchronous(self.composed_callable(*args, **inner_kwargs), **outer_kwargs)
+
+
+class GeneralizedFunction(GeneralizedCallable):
+    """A generalized callable defined by parts."""
+
+    def __init__(
+        self,
+        synchronous_callable: Optional[Callable] = None,
+        asynchronous_callable: Optional[Callable] = None,
+    ) -> None:
+        """Initialize generalized function.
+
+        Args:
+            synchronous_callable: optional synchronous body.
+            asynchronous_callable: optional asynchronous body.
+        """
+        self._synchronous_callable = synchronous_callable
+        self._asynchronous_callable = asynchronous_callable
+
+    def synchronous(self, *args: Any, **kwargs: Any) -> Any:
+        """Invoke function synchronously (ie. potentially blocking)."""
+        if self._synchronous_callable is None:
+            raise NotImplementedError("synchronous invocation is not supported")
+        return self._synchronous_callable(*args, **kwargs)
+
+    def asynchronous(self, *args: Any, **kwargs: Any) -> Any:
+        """Invoke function asynchronously, returning a future-like object."""
+        if self._asynchronous_callable is None:
+            raise NotImplementedError("asynchronous invocation is not supported")
+        return self._asynchronous_callable(*args, **kwargs)
+
+
+class GeneralizedMethod:
+    """A data descriptor for generalized callables bound to class instances."""
+
+    class Unbound:
+        """An unbound generalized method descriptor."""
+
+        def __init__(self, method: "GeneralizedMethod") -> None:
+            """Initialize unbound descriptor.
+
+            Args:
+                method: associated generalized method.
+            """
+            self.synchronous_callable: Optional[Callable] = None
+            self.asynchronous_callable: Optional[Callable] = None
+            self.transitional_callable: Optional[Callable] = None
+            if not method.transitional:
+                if inspect.iscoroutinefunction(method.prototype):
+                    self.asynchronous_callable = method.prototype
+                else:
+                    self.synchronous_callable = method.prototype
+            else:
+                self.transitional_callable = method.prototype
+            if method.synchronous_overload is not None:
+                self.synchronous_callable = method.synchronous_overload
+            if method.asynchronous_overload is not None:
+                self.asynchronous_callable = method.asynchronous_overload
+
+        def __get__(
+            self,
+            instance: Optional[Any],
+            owner: Optional[Type] = None,
+        ) -> Union["GeneralizedMethod.Unbound", "GeneralizedMethod.Bound"]:
+            if instance is None:
+                return self
+            synchronous_callable = self.synchronous_callable
+            if synchronous_callable is not None:
+                asynchronous_callable = synchronous_callable.__get__(instance, owner)
+            asynchronous_callable = self.asynchronous_callable
+            if asynchronous_callable is not None:
+                asynchronous_callable = asynchronous_callable.__get__(instance, owner)
+                if inspect.iscoroutinefunction(self.asynchronous_callable):
+                    asynchronous_callable = assign_coroutine(asynchronous_callable, instance.executor)
+            implementation = GeneralizedFunction(synchronous_callable, asynchronous_callable)
+            transitional_callable = self.transitional_callable
+            if transitional_callable is not None:
+                transitional_callable = transitional_callable.__get__(instance, owner)
+            return GeneralizedMethod.Bound(implementation, migrating_from=transitional_callable)
+
+    class Bound(VectorizingCallable, ComposableCallable):
+        """A bound generalized method callable."""
+
+        def __init__(self, body: GeneralizedCallable, migrating_from: Optional[Callable] = None) -> None:
+            """Initialize bound method callable.
+
+            Args:
+                body: method body as a generalized callable
+                migrating_from: when migrating to generalized methods,
+                the prior definition may be fed here so as to keep plain
+                method invocations the same.
+            """
+            self.body = body
+            default_callable = migrating_from
+            if default_callable is None:
+                default_callable = body.synchronous
+            self._default_callable = default_callable
+
+        def __getattr__(self, name: str) -> Any:
+            return getattr(self.body, name)
+
+        def __call__(self, *args: Any, **kwargs: Any) -> Any:
+            """Invoke method (optionally pre-existing)."""
+            return self._default_callable(*args, **kwargs)
+
+        def synchronous(self, *args: Any, **kwargs: Any) -> Any:
+            """Invoke method synchronously."""
+            return self.body.synchronous(*args, **kwargs)
+
+        def asynchronous(self, *args: Any, **kwargs: Any) -> Any:
+            """Invoke method asynchronously."""
+            return self.body.asynchronous(*args, **kwargs)
+
+    def __init__(self, prototype: Callable, transitional: bool) -> None:
+        """Initializes the generalized method.
+
+        Args:
+            prototype: method prototype, usually just a signature but
+            may also be used as an overload for convenience (iff the
+            function type matches the missing overload).
+            transitional: a transitional method will stick to its
+            prototype for default invocations, simplifying the
+            adoption of generalized methods in existing codebases.
+        """
+        self.prototype = prototype
+        self.transitional = transitional
+        self.synchronous_overload: Optional[Callable] = None
+        self.asynchronous_overload: Optional[Callable] = None
+
+    def sync_overload(self, func: Callable) -> None:
+        """Register `func` as this method synchronous overload."""
+        if self.synchronous_overload is not None:
+            raise RuntimeError("cannot redefine synchronous overload")
+        self.synchronous_overload = func
+
+    def async_overload(self, func: Callable) -> None:
+        """Register `func` as this method asynchronous overload."""
+        if self.asynchronous_overload is not None:
+            raise RuntimeError("cannot redefine asynchronous overload")
+        self.asynchronous_overload = func
+
+    def __set_name__(self, owner: Type, name: str) -> None:
+        self.__attribute_name = f"__{name}_method"
+        setattr(owner, self.__attribute_name, GeneralizedMethod.Unbound(self))
+
+    def rebind(self, instance: Any, body: GeneralizedCallable) -> None:
+        """Change this method's `body` for the given `instance`."""
+        bound_method = GeneralizedMethod.Bound(
+            body,
+            migrating_from=(self.prototype.__get__(instance) if self.transitional else None),
+        )
+        setattr(instance, self.__attribute_name, bound_method)
+
+    def __get__(
+        self,
+        instance: Optional[Any],
+        owner: Optional[Type] = None,
+    ) -> Union["GeneralizedMethod", "GeneralizedMethod.Bound"]:
+        if instance is None:
+            return self
+        return getattr(instance, self.__attribute_name)
+
+
+@overload
+def generalizedmethod(func: Callable, *, transitional: bool = ...) -> GeneralizedMethod:
+    ...
+
+
+@overload
+def generalizedmethod(*, transitional: bool = ...) -> Callable:
+    ...
+
+
+def generalizedmethod(
+    func: Optional[Callable] = None,
+    *,
+    transitional: bool = False,
+) -> Union[Callable, GeneralizedMethod]:
+    """Define a generalized method by decoration."""
+
+    def _decorator(func: Callable) -> GeneralizedMethod:
+        return GeneralizedMethod(func, transitional)
+
+    if func is None:
+        return _decorator
+    return _decorator(func)
