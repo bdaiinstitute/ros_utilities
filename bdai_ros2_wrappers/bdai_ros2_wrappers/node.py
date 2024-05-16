a411e402c46a18f4f7b32c00ced79639b6a8e9c5
Michel Hidalgo <mhidalgo@theaiinstitute.com>
2024-05-16 15:05:21 -0300

    Workaround race in Waitables handling
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

--- bdai_ros2_wrappers/bdai_ros2_wrappers/node.py
+++ bdai_ros2_wrappers/bdai_ros2_wrappers/node.py
@@ -1,11 +1,30 @@
 # Copyright (c) 2023 Boston Dynamics AI Institute Inc.  All rights reserved.
-from typing import Any, Optional
+import contextlib
+from typing import Any, Iterable, Optional, cast
 
 from rclpy.callback_groups import CallbackGroup
+from rclpy.exceptions import InvalidHandle
 from rclpy.node import Node as BaseNode
+from rclpy.wait_set import WaitSet
+from rclpy.waitables import Waitable
 
 from bdai_ros2_wrappers.callback_groups import NonReentrantCallbackGroup
 from bdai_ros2_wrappers.logging import MemoizingRcutilsLogger, as_memoizing_logger
+
+
+class SafeWaitable:
+    """A decorator to workaround https://github.com/ros2/rclpy/issues/1284."""
+
+    def __init__(self, wrapped: Waitable) -> None:
+        self._wrapped = wrapped
+
+    def __getattr__(self, name: str) -> Any:
+        return getattr(self._wrapped, name)
+
+    def add_to_wait_set(self, wait_set: WaitSet) -> None:
+        """Add waitable to `wait_set` safely."""
+        with contextlib.suppress(InvalidHandle):
+            self._wrapped.add_to_wait_set(wait_set)
 
 
 class Node(BaseNode):
@@ -39,6 +58,12 @@ class Node(BaseNode):
         """Get the default callback group."""
         # NOTE(hidmic): this overrides the hardcoded default group in rclpy.node.Node implementation
         return self._default_callback_group_override
+
+    @property
+    def waitables(self) -> Iterable[Waitable]:
+        """Get node waitables."""
+        for waitable in super().waitables:
+            yield cast(Waitable, SafeWaitable(waitable))
 
     @property
     def destruction_requested(self) -> bool:
