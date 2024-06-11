2c14e4b8dac6daaff2b006cbcb3e172be21da33c
mhidalgo-bdai <144129882+mhidalgo-bdai@users.noreply.github.com>
2024-06-11 13:18:39 -0300

    Add support for adapted message feeds (#105)
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

--- bdai_ros2_wrappers/test/test_feeds.py
+++ bdai_ros2_wrappers/test/test_feeds.py
@@ -10,6 +10,7 @@ from geometry_msgs.msg import (
 from message_filters import SimpleFilter
 
 from bdai_ros2_wrappers.feeds import (
+    AdaptedMessageFeed,
     FramedMessageFeed,
     MessageFeed,
     SynchronizedMessageFeed,
@@ -73,3 +74,23 @@ def test_synchronized_message_feed(ros: ROSAwareScope) -> None:
     pose_message, twist_message = ensure(synchronized_message_feed.latest)
     assert pose_message.pose.position.x == expected_pose_message.pose.position.x
     assert twist_message.twist.linear.x == expected_twist_message.twist.linear.x
+
+
+def test_adapted_message_feed(ros: ROSAwareScope) -> None:
+    pose_message_feed = MessageFeed(SimpleFilter())
+    position_message_feed = AdaptedMessageFeed(
+        pose_message_feed,
+        fn=lambda message: message.pose.position,
+    )
+
+    expected_pose_message = PoseStamped()
+    expected_pose_message.header.frame_id = "odom"
+    expected_pose_message.header.stamp.sec = 1
+    expected_pose_message.pose.position.x = 1.0
+    expected_pose_message.pose.position.z = -1.0
+    expected_pose_message.pose.orientation.w = 1.0
+    pose_message_feed.link.signalMessage(expected_pose_message)
+
+    position_message = ensure(position_message_feed.latest)
+    # no copies are expected, thus an identity check is valid
+    assert position_message is expected_pose_message.pose.position
