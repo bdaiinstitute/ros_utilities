41b3177b73ef9a5ecff2bd932085ac25a7841b22
mhidalgo-bdai <144129882+mhidalgo-bdai@users.noreply.github.com>
2024-06-13 17:30:46 -0300

    Fix `proto2ros.dependencies.fix_dependency_cycles` (#107)
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

--- proto2ros/proto2ros/dependencies.py
+++ proto2ros/proto2ros/dependencies.py
@@ -2,13 +2,15 @@
 
 """This module provides APIs to manipulate dependencies between Protobuf <-> ROS message equivalences."""
 
+import collections
+import itertools
 import warnings
 from typing import List
 
 from rosidl_adapter.parser import MessageSpecification
 
 from proto2ros.compatibility import networkx as nx
-from proto2ros.utilities import pairwise, to_ros_base_type
+from proto2ros.utilities import to_ros_base_type
 
 
 def message_dependency_graph(message_specs: List[MessageSpecification]) -> nx.DiGraph:
@@ -32,28 +34,46 @@ def message_dependency_graph(message_specs: List[MessageSpecification]) -> nx.Di
 def fix_dependency_cycles(message_specs: List[MessageSpecification], quiet: bool = True) -> None:
     """Fixes dependency cycles among ROS message specifications.
 
-    ROS messages do not support recursive definitions, this functions works around this
-    limitation by type erasing the thinnest link (least number of offending fields) for
-    each cycle.
+    ROS messages do not support recursive definitions, so this function works around this
+    limitation by type erasing the least amount of offending fields.
     """
     dependency_graph = message_dependency_graph(message_specs)
+
+    cycles = []
     for cycle in nx.simple_cycles(dependency_graph):
-        cycle = [*cycle, cycle[0]]  # close the loop
         if not quiet:
             message_types = [dependency_graph.nodes[node]["message"].base_type for node in cycle]
             dependency_cycle_depiction = " -> ".join(str(type_) for type_ in message_types)
+            dependency_cycle_depiction += " -> " + str(message_types[0])  # close the loop
             warnings.warn("Dependency cycle found: " + dependency_cycle_depiction, stacklevel=1)
+        cycles.append(cycle)
 
-        explicit_edges = []
-        for parent, child in pairwise(cycle):
-            message = dependency_graph.nodes[child]["message"]
-            if message.annotations["proto-class"] == "message":
-                explicit_edges.append((parent, child))
-
-        parent, child = min(explicit_edges, key=lambda edge: dependency_graph.number_of_edges(*edge))
-        for data in dependency_graph[parent][child].values():
-            field = data["field"]
-            if not quiet:
-                message_type = dependency_graph.nodes[parent]["message"].base_type
-                warnings.warn(f"Type erasing {field.name} member in {message_type} to break recursion", stacklevel=1)
-            field.annotations["type-erased"] = True
+    counter = collections.Counter(
+        sorted(
+            sorted(itertools.chain(*cycles)),  # ensures an stable order (maintained by sorted)
+            key=dependency_graph.in_degree,  # implicitly breaks ties by prioritizing the least common messages
+        ),
+    )
+    while counter.total() > 0:
+        for node, _ in counter.most_common():  # greedily break cycles
+            message = dependency_graph.nodes[node]["message"]
+            if message.annotations["proto-class"] != "message":
+                continue
+            break
+        else:
+            raise RuntimeError("no candidate for type erasure found")
+        for cycle in list(cycles):
+            if node not in cycle:
+                continue
+            parent = cycle[cycle.index(node) - 1]
+            for data in dependency_graph[parent][node].values():
+                field = data["field"]
+                if not quiet:
+                    message_type = dependency_graph.nodes[parent]["message"].base_type
+                    warnings.warn(
+                        f"Type erasing {field.type} {field.name} member in {message_type} to break recursion",
+                        stacklevel=1,
+                    )
+                field.annotations["type-erased"] = True
+            counter.subtract(cycle)
+            cycles.remove(cycle)
