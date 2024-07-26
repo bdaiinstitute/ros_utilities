a583e79fd30ccf1a4114970047871e8b16e95531
mhidalgo-bdai <144129882+mhidalgo-bdai@users.noreply.github.com>
2024-07-26 16:12:48 -0300

    Add callbacks to message feeds (#109)
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

--- bdai_ros2_wrappers/test/test_feeds.py
+++ bdai_ros2_wrappers/test/test_feeds.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2024 Boston Dynamics AI Institute Inc.  All rights reserved.
 
+from typing import Optional
 
 import tf2_ros
 from geometry_msgs.msg import (
@@ -94,3 +95,31 @@ def test_adapted_message_feed(ros: ROSAwareScope) -> None:
     position_message = ensure(position_message_feed.latest)
     # no copies are expected, thus an identity check is valid
     assert position_message is expected_pose_message.pose.position
+
+
+def test_message_feed_recalls(ros: ROSAwareScope) -> None:
+    pose_message_feed = MessageFeed(SimpleFilter())
+
+    latest_message: Optional[PoseStamped] = None
+
+    def callback(message: PoseStamped) -> None:
+        nonlocal latest_message
+        latest_message = message
+
+    conn = pose_message_feed.recall(callback)
+
+    first_pose_message = PoseStamped()
+    first_pose_message.header.stamp.sec = 1
+    pose_message_feed.link.signalMessage(first_pose_message)
+
+    assert latest_message is not None
+    assert latest_message.header.stamp.sec == first_pose_message.header.stamp.sec
+
+    conn.close()
+
+    second_pose_message = PoseStamped()
+    second_pose_message.header.stamp.sec = 2
+    pose_message_feed.link.signalMessage(second_pose_message)
+
+    assert latest_message.header.stamp.sec != second_pose_message.header.stamp.sec
+    assert latest_message.header.stamp.sec == first_pose_message.header.stamp.sec
