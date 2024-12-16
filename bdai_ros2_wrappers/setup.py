5a237cc16f12baf5abcedc404d5d59a3ce3afe9d
Tiffany Cappellari <tcappellari@theaiinstitute.com>
2024-12-16 15:33:15 +0000

    modified setup.py to install custom launch actions in /install

--- bdai_ros2_wrappers/setup.py
+++ bdai_ros2_wrappers/setup.py
@@ -1,21 +1,22 @@
 # Copyright (c) 2023 Boston Dynamics AI Institute Inc.  All rights reserved.
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = "bdai_ros2_wrappers"
 
 setup(
     name=package_name,
     version="1.0.0",
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
+    package_data={package_name: ["py.typed"]},
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
     ],
     install_requires=["setuptools"],
     maintainer="The AI Institute",
-    maintainer_email="engineering@theaiinstitute.com",
-    description="The AI Institute's wrappers for ROS2",
+    maintainer_email="opensource@theaiinstitute.com",
     zip_safe=True,
+    description="The AI Institute's wrappers for ROS 2",
     license="MIT",
 )
