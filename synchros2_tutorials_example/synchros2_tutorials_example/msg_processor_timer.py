55b4b9d9dd71b4bdf5ad226cf64ea2af56003879
Jenny Barry <116907872+jbarry-bdai@users.noreply.github.com>
2025-10-31 16:50:09 -0400

    Move synchros2 tutorials to their own package and create custom message (#164)
    ## Proposed changes
    
    This creates `synchros2_tutorials` and `synchros2_tutorials_interfaces` packages as examples in `ros_utilities`.  Since I didn't want people to have package conflict after creating their own versions I named the ones here with `_example`.
    
    Additionally, I updated the tutorials to use a custom string message instead of `std_msgs/String`.  `std_msgs/String` is deprecated and we'll need the interfaces package for the action tutorial.  However it does add a bit of complexity to `GettingStartedGuide` so I'm open to using the example messages string instead.
    
    ### Checklist
    
    <!-- Mark each checkbox as you make progress in your contribution. -->
    
    - [x] Lint and unit tests pass locally
    - [x] I have added tests that prove my changes are effective
    - [x] I have added necessary documentation to communicate the changes
    
    ### Additional comments
    
    <!-- Anything else worth mentioning. -->

--- synchros2_tutorials_example/synchros2_tutorials_example/msg_processor_timer.py
+++ synchros2_tutorials_example/synchros2_tutorials_example/msg_processor_timer.py
@@ -2,23 +2,30 @@ import threading
 import time
 
 import rclpy
-import std_msgs.msg
 
 import synchros2.process as ros_process
 import synchros2.scope as ros_scope
+import synchros2_tutorials_interfaces_example.msg
 
 
 class MsgProcessor:
+    """Example class that records a message in the subscription callback and processes it in a timer callback."""
+
     def __init__(self):
         self._node = ros_scope.ensure_node()
         self._lock = threading.Lock()
         self._raw_msg = None
         self.processed_msg = None
-        self._sub = self._node.create_subscription(std_msgs.msg.String, "chat", self._sub_callback, 1)
+        self._sub = self._node.create_subscription(
+            synchros2_tutorials_interfaces_example.msg.String,
+            "chat",
+            self._sub_callback,
+            1,
+        )
         self._timer = self._node.create_timer(timer_period_sec=0.1, callback=self._timer_callback)
         self._node.get_logger().info("Listening!")
 
-    def _sub_callback(self, msg: std_msgs.msg.String) -> None:
+    def _sub_callback(self, msg: synchros2_tutorials_interfaces_example.msg.String) -> None:
         self._node.get_logger().info(f"Callback received message {msg}")
         # Store the message
         with self._lock:
@@ -32,13 +39,14 @@ class MsgProcessor:
             new_msg = self._raw_msg
             self._raw_msg = None
         start = self._node.get_clock().now()
-        time.sleep(10)
+        time.sleep(5)
         self.processed_msg = f"{new_msg}: {start} -> {self._node.get_clock().now()}"
         self._node.get_logger().info(f"Set processed message to {self.processed_msg}")
 
 
 @ros_process.main()
 def main() -> None:
+    """Main function that prints the processed message once per second."""
     mp = MsgProcessor()
     node = ros_scope.ensure_node()
     rate = node.create_rate(1.0)
