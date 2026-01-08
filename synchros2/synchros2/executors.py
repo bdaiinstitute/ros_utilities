e5e6c02f582d398bb83b870811c9a03ca62cfce7
Max Schik <schik@fzi.de>
2026-01-08 10:35:33 +0000

    fixes binding of spin_once to thread
    Signed-off-by: Max Schik <schik@fzi.de>

--- synchros2/synchros2/executors.py
+++ synchros2/synchros2/executors.py
@@ -831,6 +831,10 @@ def background(executor: rclpy.executors.Executor) -> typing.Iterator[rclpy.exec
     executor.spin = bind_to_thread(executor.spin, background_thread)
     executor.spin_once = bind_to_thread(executor.spin_once, background_thread)
     executor.spin_until_future_complete = bind_to_thread(executor.spin_until_future_complete, background_thread)
+    executor.spin_once_until_future_complete = bind_to_thread(
+        executor.spin_once_until_future_complete,
+        background_thread,
+    )
     background_thread.start()
     try:
         yield executor
